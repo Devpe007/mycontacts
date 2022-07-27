a5c8e2b4b5cc243985f32f4b66f3faa7eec951bb
Devpe007 <anddev007pe@gmail.com>
2022-07-27 00:11:46 -0300

    create Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import {
+  Container,
+  InputSearchContainer,
+} from './styles';
+
+import logo from '../../assets/images/logo-mycontacts.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <img
+        src={logo}
+        alt="MyContacts"
+        width="201"
+      />
+
+      <InputSearchContainer>
+        <input
+          type="text"
+          placeholder="Pesquise pelo nome..."
+        />
+      </InputSearchContainer>
+    </Container>
+  );
+};
