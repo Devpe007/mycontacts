4f84b9c010eda6dc9568d3d052a79fb6a46602d6
Devpe007 <anddev007pe@gmail.com>
2022-08-23 07:29:46 -0300

    create Select component

---
+++ src/components/Select.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  padding: 0 16px;
+
+  width: 100%;
+  height: 52px;
+
+  border-radius: 4px;
+  border: none;
+  border: 2px solid #fff;
+
+  outline: none;
+
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+
+  font-size: 16px;
+
+  background: #fff;
+
+  transition: border 0.2s ease-in;
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+  }
+`;
