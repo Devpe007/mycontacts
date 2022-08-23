b51f9d1385d530177ab60f91594861302a1add49
Devpe007 <anddev007pe@gmail.com>
2022-08-23 07:40:45 -0300

    create Button component

---
+++ src/components/Button.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  width: 100%;
+  height: 52px;
+
+  border: none;
+  border-radius: 4px;
+
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+
+  font-size: 16px;
+  font-weight: bold;
+
+  color: #fff;
+  background: ${({ theme }) => theme.colors.primary.main};
+
+  transition: background 0.2s ease-in;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.primary.light};
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.colors.primary.dark};
+  }
+
+  &[disabled] {
+    cursor: default;
+
+    background: #ccc;
+  }
+`;
