504d2e03956dd2e303cb3b9262f97f2ab6ef2885
Devpe007 <anddev007pe@gmail.com>
2022-08-23 08:07:26 -0300

    create ContactForm component structure

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import {
   Container,
   InputSearchContainer,
@@ -22,7 +24,7 @@ export default function Home() {
 
       <Header>
         <strong>3 contatos</strong>
-        <a href="/">Novo contato</a>
+        <Link to="/new">Novo contato</Link>
       </Header>
 
       <ListContainer>
@@ -56,80 +58,12 @@ export default function Home() {
           <div
             className="actions"
           >
-            <a href="/">
+            <Link to="/edit/123">
               <img
                 src={edit}
                 alt="Edit"
               />
-            </a>
-            <button
-              type="button"
-            >
-              <img
-                src={trash}
-                alt="Delete"
-              />
-            </button>
-          </div>
-        </Card>
-        <Card>
-          <div
-            className="info"
-          >
-            <div
-              className="contact-name"
-            >
-              <strong>Mateus Silva</strong>
-              <small>instagram</small>
-            </div>
-
-            <span>mateus@devacademy.com.br</span>
-            <span>(41) 99999-9999</span>
-          </div>
-
-          <div
-            className="actions"
-          >
-            <a href="/">
-              <img
-                src={edit}
-                alt="Edit"
-              />
-            </a>
-            <button
-              type="button"
-            >
-              <img
-                src={trash}
-                alt="Delete"
-              />
-            </button>
-          </div>
-        </Card>
-        <Card>
-          <div
-            className="info"
-          >
-            <div
-              className="contact-name"
-            >
-              <strong>Mateus Silva</strong>
-              <small>instagram</small>
-            </div>
-
-            <span>mateus@devacademy.com.br</span>
-            <span>(41) 99999-9999</span>
-          </div>
-
-          <div
-            className="actions"
-          >
-            <a href="/">
-              <img
-                src={edit}
-                alt="Edit"
-              />
-            </a>
+            </Link>
             <button
               type="button"
             >
