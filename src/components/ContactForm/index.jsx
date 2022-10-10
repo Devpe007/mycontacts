16e5ccc0f01a7291f166422f3d11027c89870d3d
Devpe007 <anddev007pe@gmail.com>
2022-10-10 15:38:25 -0300

    clearning form data after submission

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -89,6 +89,11 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
     });
 
     setIsSubmitting(false);
+
+    setName('');
+    setEmail('');
+    setPhone('');
+    setCategoryId('');
   };
 
   return (
