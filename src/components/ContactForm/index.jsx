18c42dd08bda126c4c62b29949e90f6108b28cd6
Devpe007 <anddev007pe@gmail.com>
2022-10-13 23:30:18 -0300

    implementing contact editing

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -103,11 +103,6 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
     });
 
     setIsSubmitting(false);
-
-    setName('');
-    setEmail('');
-    setPhone('');
-    setCategoryId('');
   };
 
   return (
