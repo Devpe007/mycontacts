700cc9ef130abb207a392de809b0ce3ee109c9c9
Devpe007 <anddev007pe@gmail.com>
2022-10-13 23:45:51 -0300

    refactoring form reset on create page

--- src/pages/NewContact/index.jsx
+++ src/pages/NewContact/index.jsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
 
@@ -6,6 +8,8 @@ import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
 
 export default function NewContact() {
+  const contactFormRef = useRef(null);
+
   async function handleSubmit(formData) {
     try {
       const contact = {
@@ -16,6 +20,8 @@ export default function NewContact() {
       };
 
       await ContactsService.createContact(contact);
+
+      contactFormRef.current.resetFields();
 
       toast({
         type: 'success',
@@ -36,6 +42,7 @@ export default function NewContact() {
       />
 
       <ContactForm
+        ref={contactFormRef}
         buttonLabel="Cadastrar"
         onSubmit={handleSubmit}
       />
