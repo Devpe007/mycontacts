f45cbc31a2b10ce915c6f93c5be1789fb107672f
Devpe007 <anddev007pe@gmail.com>
2022-11-16 07:37:19 -0300

    componentizing the ContactsList from the Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,6 @@
-import { Link } from 'react-router-dom';
-
 import {
   Container,
-  ListHeader,
-  Card,
 } from './styles';
-
-import arrow from '../../assets/images/icons/arrow.svg';
-import edit from '../../assets/images/icons/edit.svg';
-import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
 import Modal from '../../components/Modal';
@@ -18,6 +10,7 @@ import Header from './components/Header';
 import ErrorStatus from './components/ErrorStatus';
 import EmptyList from './components/EmptyList';
 import SearchNotFound from './components/SearchNotFound';
+import ContactsList from './components/ContactsList';
 
 import useHome from './useHome';
 
@@ -71,62 +64,12 @@ export default function Home() {
             />
           )}
 
-          {filteredContacts.length > 0 && (
-          <ListHeader
+          <ContactsList
+            filteredContacts={filteredContacts}
             orderBy={orderBy}
-          >
-            <button
-              type="button"
-              onClick={handleToggleOrderBy}
-            >
-              <span>Nome</span>
-              <img
-                src={arrow}
-                alt="Arrow"
-              />
-            </button>
-          </ListHeader>
-          )}
-
-          {filteredContacts.map((contact) => (
-            <Card key={contact.id}>
-              <div
-                className="info"
-              >
-                <div
-                  className="contact-name"
-                >
-                  <strong>{contact.name}</strong>
-                  {contact.category.name && (
-                  <small>{contact.category.name}</small>
-                  )}
-                </div>
-
-                <span>{contact.email}</span>
-                <span>{contact.phone}</span>
-              </div>
-
-              <div
-                className="actions"
-              >
-                <Link to={`/edit/${contact.id}`}>
-                  <img
-                    src={edit}
-                    alt="Edit"
-                  />
-                </Link>
-                <button
-                  type="button"
-                  onClick={() => handleDeleteContact(contact)}
-                >
-                  <img
-                    src={trash}
-                    alt="Delete"
-                  />
-                </button>
-              </div>
-            </Card>
-          ))}
+            onToggleOrderBy={handleToggleOrderBy}
+            onDeleteContact={handleDeleteContact}
+          />
 
           <Modal
             danger
@@ -139,7 +82,6 @@ export default function Home() {
           >
             <p>Esta ação não poderá ser desfeita!</p>
           </Modal>
-
         </>
       )}
     </Container>
