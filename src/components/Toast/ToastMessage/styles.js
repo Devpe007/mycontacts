a2dbaf568c54dc02251f91521c8d4e489423e7ef
Devpe007 <anddev007pe@gmail.com>
2022-11-16 09:36:18 -0300

    animating component input into lists

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,4 +1,18 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+const messageIn = keyframes`
+  from {
+    opacity: 0;
+
+    transform: translateY(100px);
+  }
+
+  to {
+    opacity: 1;
+
+    transform: translateY(0px);
+  }
+`;
 
 const containerVariants = {
   default: css`
@@ -26,6 +40,8 @@ export const Container = styled.div`
   cursor: pointer;
 
   color: #ffffff;
+
+  animation: ${messageIn} 0.3s;
 
   ${({ type }) => containerVariants[type] || containerVariants.default}
 
